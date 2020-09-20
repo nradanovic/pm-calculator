38c08d344925df00226198ffbb84c28dd0f7ece8
Neno Radanović <neno.radanovic@aktivis.hr>
2020-09-20 21:23:59 +0200

    Updates

---
+++ precache-manifest.6131131e825a6db3c72c4f83da18e84f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8405ab31e6c36e2837dc3637a6bc67b",
+    "url": "/pm-calculator/index.html"
+  },
+  {
+    "revision": "71d8316495b1c227bb49",
+    "url": "/pm-calculator/static/css/main.0cdf315c.chunk.css"
+  },
+  {
+    "revision": "17a420bc5a9f8f51fe8d",
+    "url": "/pm-calculator/static/js/2.c67670b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pm-calculator/static/js/2.c67670b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d8316495b1c227bb49",
+    "url": "/pm-calculator/static/js/main.88d149b7.chunk.js"
+  },
+  {
+    "revision": "83dd704b9fae01fb4f03",
+    "url": "/pm-calculator/static/js/runtime-main.61330e5f.js"
+  }
+]);
